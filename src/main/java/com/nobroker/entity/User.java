d3f0c571a21c73bd8b48aeb83d99d2d92027ce69
Stark-gif <you@example.com>
2024-01-23 09:10:32 +0530

    23rd jan 2024

--- src/main/java/com/nobroker/entity/User.java
+++ src/main/java/com/nobroker/entity/User.java
@@ -1,21 +1,24 @@
 package com.nobroker.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String email;
-    private String mobile;
     private String password;
+    private String mobile;
+    private boolean emailVerified;
 }
--- src/main/java/com/nobroker/service/UserService.java
+++ src/main/java/com/nobroker/service/UserService.java
@@ -1,7 +1,28 @@
 package com.nobroker.service;
 
-import com.nobroker.payload.UserDto;
+import com.nobroker.entity.User;
+import com.nobroker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    public long createUser(UserDto userDto);
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepo;
+
+
+    public User registerUser(User user){
+
+        return userRepo.save(user);
+    }
+
+
+    public User getUserByEmail(String email) {
+        return userRepo.findByEmail(email);
+    }
+
+    public void verifyEmail(User user) {
+        user.setEmailVerified(true);
+        userRepo.save(user);
+    }
 }
